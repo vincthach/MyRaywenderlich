114d90663883cb163559a6fb71c62a4522f1378e
ThachNguyen <pcngocthach@gmail.com>
2018-07-18 10:44:13 +0700

    Initial Commit

---
+++ RaywenderlichPart1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RaywenderlichPart1
+//
+//  Created by ThachNguyen on 7/18/18.
+//  Copyright © 2018 ThachNguyen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

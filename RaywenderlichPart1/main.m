0b17596a851733e819eb2c99cf788c238f7f12f8
ThachNguyen <pcngocthach@gmail.com>
2018-07-18 14:04:04 +0700

    Add loop do while for continue add

--- RaywenderlichPart1/main.m
+++ RaywenderlichPart1/main.m
@@ -10,9 +10,15 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        People *people = [[People alloc] init];
-        [people enterInfo];
-        [people printInfo];
+        char response;
+        do {
+            People *people = [[People alloc] init];
+            [people enterInfo];
+            [people printInfo];
+            
+            NSLog(@"Do you want to enter another name? (y/n)");
+            scanf("\n%c", &response);
+        } while (response == 'y');
     }
     return 0;
 }
